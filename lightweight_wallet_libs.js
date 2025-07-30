d320ab6556497682678de9254a7914c455173f51
Krakaw <41575888+Krakaw@users.noreply.github.com>
2025-07-30 16:58:07 +0000

    Deploy WASM module and web interface

--- lightweight_wallet_libs.js
+++ lightweight_wallet_libs.js
@@ -830,7 +830,7 @@ function __wbg_get_imports() {
         const ret = false;
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper555 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper553 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 114, __wbg_adapter_46);
         return ret;
     };
